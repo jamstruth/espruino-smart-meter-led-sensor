b36a0bd6284cbb5a370e4c88a1f9e9ce7af27aba
James Struthers <jamstruth18@gmail.com>
2025-04-08 21:18:27 +0100

    Updating to use LED light meter

--- meter.js
+++ meter.js
@@ -1,16 +1,18 @@
 var counter = 0; // number of impulses
 var power = 0; // power in watts (assuming 1000imp/kwh)
 var lastPulse = getTime();
+var pulsePerKwh = 1000;
 
 // Update BLE advertising
 function update() {
+  print("Updating Advert");
   NRF.setAdvertising(require("BTHome").getAdvertisement([
     {
       type : "battery",
       v : E.getBattery()
     }, {
       type : "energy",
-      v : counter/1000 // kWh, and usualy 1000 imp/kWh
+      v : counter/pulsePerKwh // kWh, and usualy 1000 imp/kWh
     }, {
       type : "power",
       v : power // kWh, and usualy 1000 imp/kWh
@@ -18,20 +20,31 @@ function update() {
   ]), {
     name : "Electricity",
     // not being connectable/scannable saves power (but you'll need to reboot to connect again with the IDE!)
-    //connectable : false, scannable : false,
+    connectable : true, scannable : true, whenConnected: true,
   });
 }
 
-// Set up pin states
-D1.write(0);
-pinMode(D2,"input_pullup");
+// D5 (LED1) is used for sensing
+// D1 is used as an output which is also watched with setWatch to detect a state change
+var ll = require("NRF52LL");
+analogRead(D5);
+// set up D1 as an output
+digitalWrite(D1,0);
+// create a 'toggle' task for pin D1
+var tog = ll.gpiote(7, {type:"task",pin:D1,lo2hi:1,hi2lo:1,initialState:0});
+// compare D5 against vref/16  (vref:8 would be vref/2)
+var comp = ll.lpcomp({pin:D5,vref:4,hyst:true});
+// use a PPI to trigger the toggle event
+ll.ppiEnable(0, comp.eCross, tog.tOut);
+// Detect a change on D1
 // Watch for pin changes
 setWatch(function(e) {
   let timeDiff = e.time - lastPulse;
-  power = 1000 / timeDiff; // 1000imp/kwh -> 1000 pulses in 3600sec = 1kW
+  power = 3600 / timeDiff; // 1000imp/kwh -> 1000 pulses in 3600sec = 1kW
   counter++;
   update();
   lastPulse = e.time;
-  digitalPulse(LED1,1,1); // show activity
-}, D2, { repeat:true, edge:"falling" });
+  print('1Wh Detected');
+}, D1, { repeat:true, edge:"falling" });
+
 update();
